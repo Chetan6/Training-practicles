cc514e8aa3f69e18685e86bbea4ac96b5feaf75d
chetan6 <chetan.kaushik@berylsystems.com>
2023-06-08 16:38:15 +0530

    find 13 friday after given date

--- year_month_friday13.rb
+++ year_month_friday13.rb
@@ -1,6 +1,35 @@
 #47. Given the month and year as numbers, return whether that month contains a Friday 13th
 
 
+def demo(yr,m,d)
+    time=Time.new(yr,m,d)
+    puts time 
+    ok= time.wday
+
+    if ok<5
+         a=5-ok 
+         puts a+13*7
+    elsif ok==5
+        puts 13*7
+    else
+         puts 13*7+5
+    end 
+end 
+
+print 'enter the year:'
+yr=gets.chomp.to_i 
+
+print 'enter the month:'
+m=gets.chomp.to_i 
+
+print 'enter the day:'
+d=gets.chomp.to_i 
+
+demo(yr,m,d)
+
+
+
+=begin        
 def fun(yr,m)
     time=Time.new(yr,m) 
     puts time
@@ -26,3 +55,4 @@ def fun(yr,m)
   m=gets.chomp.to_i 
 
    fun(yr,m)
+=end 
